a14be816fa47235d9544bfb0505303f50057e365
Dawid Malinowski <d.malinowski@oberthur.com>
2015-10-16 00:19:04 +0200

    Add print

--- start-haproxy.sh
+++ start-haproxy.sh
@@ -106,4 +106,9 @@ if [ "$LOGGING" == "enabled" ]; then
     (rsyslogd -n &)
 fi
 
+# should we pring config
+if [ "PRINT_CONFIG" == "enabled" ]; then
+    cat /etc/haproxy/haproxy.cfg
+fi
+
 exec haproxy -db -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
