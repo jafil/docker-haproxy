2898d2a83e2bba9eadef85b678391520d38c72ec
Dawid Malinowski <dawidmalina@gmail.com>
2015-10-19 20:42:31 +0200

    Create start-sample.sh

---
+++ start-sample.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# should we enable logging
+if [ "${LOGGING}" == "enabled" ]; then
+    (rsyslogd -n &)
+fi
+
+# should we pring config
+if [ "${PRINT_CONFIG}" == "enabled" ]; then
+    env |grep BACKEND_
+    cat /etc/haproxy/haproxy.cfg
+fi
+
+exec haproxy -db -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid
