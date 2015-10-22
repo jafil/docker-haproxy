984c15e4f36138a9a5c5bc7e6fcc84a2adb83c52
Dawid Malinowski <d.malinowski@oberthur.com>
2015-10-22 17:10:12 +0200

    Move to better extract left side

--- start-haproxy.sh
+++ start-haproxy.sh
@@ -37,7 +37,7 @@ fi
 # generate acl rules
 for BACKEND in $( env |grep BACKEND_ |sort |awk 'match($0, /BACKEND_[0-9]+/) { print substr( $0, RSTART, RLENGTH )}' |uniq )
 do
-    ACL=$( env |grep ${BACKEND} |grep ACL |sed 's/BACKEND_.*=//' )
+    ACL=$( env |grep ${BACKEND} |grep ACL |sed 's/^[^=]*=//' )
 
     if [ "${ACL}" != "" ]; then
         echo "    acl is_${BACKEND} ${ACL}" >> /etc/haproxy/haproxy.cfg
@@ -54,19 +54,19 @@ do
   do
     case "$ELEMENT" in
       *ADDRESS*)
-        ADDRESS=$( echo $ELEMENT |sed 's/BACKEND_.*=//' )
+        ADDRESS=$( echo $ELEMENT |sed 's/^[^=]*=//' )
         ;;
       *PORT*)
-        PORT=$( echo $ELEMENT |sed 's/BACKEND_.*=//' )
+        PORT=$( echo $ELEMENT |sed 's/^[^=]*=//' )
         ;;
       *CERTIFICATE*)
-        CERTIFICATE=$( echo $ELEMENT |sed 's/BACKEND_.*=//' )
+        CERTIFICATE=$( echo $ELEMENT |sed 's/^[^=]*=//' )
         ;;
       *OVERRIDE_HOST*)
-        OVERRIDE_HOST=$( echo $ELEMENT |sed 's/BACKEND_.*=//' )
+        OVERRIDE_HOST=$( echo $ELEMENT |sed 's/^[^=]*=//' )
         ;;
       *SSL*)
-        SSL=$( echo $ELEMENT |sed 's/BACKEND_.*=//' )
+        SSL=$( echo $ELEMENT |sed 's/^[^=]*=//' )
         ;;
     esac
   done
@@ -89,23 +89,23 @@ do
       DNS="resolvers docker resolve-prefer ipv4"
   fi
 
-  # override host header
-  if [ "${OVERRIDE_HOST}" != "" ]; then
-     echo "    reqirep ^Host: Host:\ ${OVERRIDE_HOST}" >> /etc/haproxy/haproxy.cfg
-  fi
-
   # generate reqrep rules
-  REQREP1=$( env |grep ${BACKEND} |grep REQREP_1 |sed 's/BACKEND_.*=//' )
+  REQREP1=$( env |grep ${BACKEND} |grep REQREP_1 |sed 's/^[^=]*=//' )
   if [ "${REQREP1}" != "" ]; then
      echo "    ${REQREP1}" >> /etc/haproxy/haproxy.cfg
   fi
-  REQREP2=$( env |grep ${BACKEND} |grep REQREP_2 |sed 's/BACKEND_.*=//' )
+  REQREP2=$( env |grep ${BACKEND} |grep REQREP_2 |sed 's/^[^=]*=//' )
   if [ "${REQREP2}" != "" ]; then
      echo "    ${REQREP2}" >> /etc/haproxy/haproxy.cfg
   fi
-  REQREP3=$( env |grep ${BACKEND} |grep REQREP_3 |sed 's/BACKEND_.*=//' )
+  REQREP3=$( env |grep ${BACKEND} |grep REQREP_3 |sed 's/^[^=]*=//' )
   if [ "${REQREP3}" != "" ]; then
      echo "    ${REQREP3}" >> /etc/haproxy/haproxy.cfg
+  fi
+
+  # override host header
+  if [ "${OVERRIDE_HOST}" != "" ]; then
+     echo "    reqirep ^Host: Host:\ ${OVERRIDE_HOST}" >> /etc/haproxy/haproxy.cfg
   fi
 
   echo "    server ${BACKEND} ${ADDRESS}:${PORT} check ${PARAMS} ${CERT} ${DNS}" >> /etc/haproxy/haproxy.cfg
@@ -123,7 +123,7 @@ if [ "$( env |grep RESOLVER_ | wc -l )" != "0" ]; then
               do
                 case "$ELEMENT" in
                   *VALUE*)
-                    VALUE=$( echo $ELEMENT |sed 's/RESOLVER_.*=//' )
+                    VALUE=$( echo $ELEMENT |sed 's/^[^=]*=//' )
                     ;;
                 esac
               done
